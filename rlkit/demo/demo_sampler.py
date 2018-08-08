259fc34966f4e2356093668700cb2ffdcced1956
amandlek <ajay.mandlekar@gmail.com>
2018-08-07 19:34:33 -0700

    support for demos

---
+++ rlkit/demo/demo_sampler.py
@@ -0,0 +1,84 @@
+"""
+A class that is responsible for sampling batches from demonstrations 
+as if they came from a replay buffer.
+"""
+
+import random
+import numpy as np
+
+class DemoSampler(object):
+    def __init__(self, demo_path, observation_dim, action_dim, preload=False):
+        """
+        :param demo_path: Path to a demo pkl file. Should also have a corresponding
+                          bkl file located there.
+        :param observation_dim: size of observations
+        :param action_dim: size of actions
+        :param preload: If true, preload the huge bkl file instead of lazily loading.
+        """
+
+        self.observation_dim = observation_dim
+        self.action_dim = action_dim
+        self.preload = preload
+
+        with open(demo_path, "rb") as small_pkl:
+            self.offsets = pickle.load(demo_path)
+        assert(self.offsets[0] == 0)
+
+        self.demo_big_file = open(demo_path.replace('.pkl', '.bkl'), "rb")
+        self.demo_data = None
+        if preload:
+            data = []
+            for ofs in self.offsets:
+                self.demo_big_file.seek(ofs)
+                data.extend(pickle.load(self.demo_big_file))
+            self.demo_data = data # stores all data in a huuuge buffer
+            self.demo_length = len(data)
+            self.demo_big_file.close()
+            self.demo_big_file = None
+            print("Demo length is {}".format(self.demo_length))
+
+
+    ### TODO: time how long it takes to get a batch ###
+    def _uniform_sample(self):
+        # NOTE: this is not truly uniform... 
+        if self.preload:
+            return random.choice(self.demo_data)
+        else:
+            # first sample an episode, then a point in time
+            episode_choice = random.choice(self.offsets) 
+            self.demo_big_file.seek(episode_choice)
+            episode = pickle.load(self.demo_big_file)
+            return random.choice(episode)
+
+    def get_batch(batch_size):
+        """
+        Returns a dictionary of batched (s, a, r, s', done) arrays.
+        """
+
+        if self.preload:
+            indices = np.random.randint(0, self.demo_length, batch_size)
+            choices = [self.demo_data[ind] for ind in indices]
+        else:
+            choices = [self._uniform_sample() for _ in range(batch_size)]
+
+        obs = np.zeros((batch_size, self.observation_dim))
+        acs = np.zeros((batch_size, self.action_dim))
+        next_obs = np.zeros((batch_size, self.observation_dim))
+        rewards = np.zeros((batch_size, 1))
+        terminals = np.zeros((batch_size, 1), dtype='uint8')
+
+        for i in range(batch_size):
+            o, a, no, r, t = choices[i]
+            obs[i] = o
+            acs[i] = a
+            next_obs[i] = no
+            rewards[i] = r
+            terminals[i] = int(t)
+
+        return dict(
+                    observations=obs,
+                    actions=acs,
+                    rewards=rewards,
+                    terminals=terminals,
+                    next_observations=next_obs,
+                )
---
+++ rlkit/demo/extract_sars.py
@@ -0,0 +1,106 @@
+"""
+Script for extacting (s, a, r, s', done) pairs
+from demo files.
+
+It will output two pickle files.
+"""
+
+import os
+import pickle
+import numpy as np
+from MujocoManip.miscellaneous.utils import postprocess_model_xml
+from rlkit.envs.mujoco_manip_env import MujocoManipEnv
+
+TASK = "SawyerPegsRoundEnv"
+DEMO_FILE = "/Users/ajaymandlekar/Desktop/pegs-RoundNut0.pkl"
+OUT_FILE = "/Users/ajaymandlekar/Desktop/pegs-RoundNut0-sars.pkl"
+
+if __name__ == "__main__":
+    env = MujocoManipEnv(TASK)
+    # env = make(TASK,
+    #            ignore_done=True,
+    #            has_renderer=True,
+    #            use_camera_obs=False,
+    #            gripper_visualization=False,
+    #            show_gripper_visualization=False,
+    #            render_visual_mesh=True,
+    #            reward_shaping=True)
+
+    # load small pickle file (offsets) and big pickle file (data)
+    with open(DEMO_FILE,'rb') as f:
+        d = pickle.load(f)
+        # a hacky way to tell if this is a small pickle files with indices into 
+        # a bigger one, or if this is a pickle file with demos in it.
+        if d[0] == 0:
+            big = open(DEMO_FILE.replace('.pkl','.bkl'), 'rb')
+        else:
+            big = None
+
+    # We'll maintain a big pickle file of all data and a small offsets array that
+    # will be stored in a smaller pickle file.
+    fname = OUT_FILE.replace('.pkl', '.bkl')
+    big_pkl = open(fname, 'wb')
+    offsets = [0]
+    current_offset = 0
+    for i, t in enumerate(d):
+
+        if type(t) == int:
+            big.seek(t)
+            t = pickle.load(big)
+
+        try:
+            # env.env.reset()
+            env.env.reset()
+            env.env.reset_from_xml_string(postprocess_model_xml(t['model.xml']))
+
+            # list of (s, a, r, s', done) tuples
+            sarsd = []
+            s = None
+            a = None
+            r = None
+            ns = None
+
+            for mj_state, ad in zip(t['states'], t['additional_data']):
+
+                # force simulation state to recorded state
+                env.env.sim.set_state_from_flattened(mj_state)
+                env.env.sim.forward()
+
+                # extract state and record (s, a, r, s') for previous timestep
+                ob_dict = env.env._get_observation()
+                ns = env._flatten_obs(ob_dict)
+                if s is not None:
+                    sarsd.append((s, a, r, ns, 0))
+                s = np.array(ns)
+
+                # extract action and reward
+                gripper_pos = [0., 0.] if ad['gripper_cmd'] else [1., -1.]
+                a = np.array(ad['controller_info']['joint_vel'].tolist() + gripper_pos)
+                r = env.env.reward(None)
+
+                # print(r)
+                # env.env.render()
+
+            print("final reward for demo {}: {}".format(i, r))
+            # last state is treated as terminal
+            r += 1. # boost artificially since sparse reward did not work?
+            sarsd.append((s, a, r, ns, 1))
+
+            # write pickle object to big pickle file, and keep track of offsets
+            raw = pickle.dumps(sarsd)
+            delta_offset = big_pkl.write(raw)
+            current_offset += delta_offset
+            offsets.append(current_offset)
+
+        except Exception as e:
+            print("ERROR on demo {}".format(i))
+            print("ERROR: {}".format(e))
+
+    # finally, write all of the offsets to the small pickle file
+    small_pkl = open(OUT_FILE, 'wb')
+    pickle.dump(offsets, small_pkl)
+    big_pkl.close()
+    small_pkl.close()
+
+
+
