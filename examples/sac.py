e52b357bb902c5ec4a489c05fbc535d1d216e404
AJ <ajay.mandlekar@gmail.com>
2018-08-09 14:48:47 -0700

    some minor edits

--- examples/sac.py
+++ examples/sac.py
@@ -20,13 +20,15 @@ from rlkit.envs.mujoco_manip_env import MujocoManipEnv
 USE_GPU = True
 U.set_gpu_mode(USE_GPU)
 
-EXPERIMENT_NAME = "pegs-reward-scale-0.5-updates-100"
+EXPERIMENT_NAME = "cans-50-50-reward-scale-1"
+#EXPERIMENT_NAME = "pegs-50-50-reward-scale-0.1"
+#EXPERIMENT_NAME = "lift-lr-1e-4"
 HORIZON = 250
-UPDATES_PER_STEP = 100 # 1
-REWARD_SCALE = 0.5
+UPDATES_PER_STEP = 1
+REWARD_SCALE = 1
 
 # DEMO_PATH = None
-DEMO_PATH = "/home/robot/Downloads/pegs-RoundNut0-sars.pkl"
+DEMO_PATH = "/home/robot/Downloads/test_extraction/bins-Can0-sars.pkl"
 
 ACTION_SKIP = 1
 LR = 3E-4
@@ -38,7 +40,7 @@ def experiment(variant):
     # env = NormalizedBoxEnv(gym.make('HalfCheetah-v2'))
     # env = gym.make('HalfCheetah-v2')
 
-    env = MujocoManipEnv("SawyerPegsRoundEnv") # wrap as a gym env
+    env = MujocoManipEnv("SawyerBinsCanEnv") # wrap as a gym env
     obs_dim = int(np.prod(env.observation_space.shape))
     action_dim = int(np.prod(env.action_space.shape))
 
